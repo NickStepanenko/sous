b69c8e0a5203119884ebb2050c894eceb657f4f6
Nick Stepanenko <massaalonso@yandex.ru>
2015-05-19 01:44:20 +0800

    Removed insignificant program code.

--- main.js
+++ main.js
@@ -4,55 +4,8 @@
 
 var fs = require('fs');
 var cp = require('child_process');
-var data = [];
 
 var async = require('async');
-    /*tasks = [
-        function (callback) {
-            cp.exec('git log', function(error, stdout, stderr) {
-                var listOfCommits = [];
-                var authors = findData(stdout, "Author:");
-                var dates = findData(stdout, "Date:");
-                var comments = findComments(stdout);
-                var commitNumbers = findCommitNumbers(stdout);
-
-                for(i=0; i<authors.length; i++) {
-                    var commit = {
-                        "commit": commitNumbers[i],
-                        "author": authors[i],
-                        "date": dates[i],
-                        "comment": comments[i],
-                        "content": 'Original'
-                    };
-
-                    listOfCommits[listOfCommits.length] = commit;
-                }
-
-                data = listOfCommits.reverse();
-
-                callback(null, data);
-
-                if (error !== null) {
-                    console.log('stderr: ' + stderr);
-                    console.log('exec error: ' + error);
-                }
-            });
-        }
-    ],
-    secondTasks = [
-        function some_function(callback) {
-            cp.exec('git diff', function(error, stdout, stderr) {
-                var currentContent = stdout;
-
-                if (error !== null) {
-                    console.log('stderr: ' + stderr);
-                    console.log('exec error: ' + error);
-                }
-
-                callback(null, currentContent);
-            });
-        }
-    ];*/
 
 function findData(text, keyword) {
     var data = [];
@@ -91,43 +44,8 @@ function findCommitNumbers(text) {
     return commits;
 }
 
-/*function pullCommitContent(first, second) {
-
-    cp.exec('git diff' + ' ' + first + ' ' + second, function(error, stdout, stderr) {
-        var currentContent = stdout;
-
-        if (error !== null) {
-            console.log('stderr: ' + stderr);
-            console.log('exec error: ' + error);
-        }
-
-        return currentContent;
-    });
-}*/
-
-var commitsInfo = [];
-
 fs.exists('.git', function (exists) {
     if(exists) {
-        /*async.series(tasks, function(err, result) {
-            fs.writeFileSync("patches.sousp", JSON.stringify(result));
-
-            var data = result[0];
-
-            /*function some_function(arg1, arg2, callback) {
-                cp.exec('git diff' + ' ' + arg1 + ' ' + arg2, function(error, stdout, stderr) {
-                    var currentContent = 54321;//stdout;
-
-                    if (error !== null) {
-                        console.log('stderr: ' + stderr);
-                        console.log('exec error: ' + error);
-                    }
-
-                    callback(null, currentContent);
-                });
-            }
-        });*/
-
         async.waterfall([
             function getCommitsInfo(callback) {
                 cp.exec('git log', function(error, stdout, stderr) {
