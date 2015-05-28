ffcf4e05cdf768907baf6533584bb7e5115063fc
NickStepanenko <massaalonso@yandex.ru>
2015-05-28 22:52:52 +0800

    Data write in patches.json now.

--- sous.js
+++ sous.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /**
  * Created by Nick on 17.05.2015.
  */
@@ -7,55 +9,30 @@ var cp = require('child_process');
 
 var async = require('async');
 
-function readyStringToJSON(stringData) {
-    var obj = {};
-    var data = stringData.split(',"');
-
-    obj.commit = data[0].slice(data[0].indexOf(':')+2, data[0].length-1);
-    obj.author = data[1].slice(data[1].indexOf(':')+2, data[1].length-1);
-    obj.date = data[2].slice(data[2].indexOf(':')+2, data[2].length-1);
-    obj.comment = data[3].slice(data[3].indexOf(':')+2, data[3].length-1);
-    obj.content = data[4].slice(data[4].indexOf(':')+2, data[4].length-1);
-
-    return obj;
-}
-
 function nextState(commitNum, direction) {
-    var data = fs.readFileSync('patches.sousp').toString().split('},{"');
+    var data = fs.readFileSync('patches.json').toString();
+    data = JSON.parse(data);
     var newState = {};
 
-    for(i=0; i<data.length; i++) {
-        if(i==0) {
-            data[i] = data[i].slice(1);
-        } else if(i!=data.length-1) {
-            data[i] = '"' + data[i];
-        } else if(i==data.length-1) {
-            data[i] = '"' + data[i].slice(0, data[i].length-2);
-        }
-        data[i] = readyStringToJSON(data[i]);
-    }
-
-    console.log(data);
-
-    for(i=0; i<data.length; i++) {
-        if(data[i].commit == commitNum) {
+    for(i=0; i<data.commits.length; i++) {
+        if(data.commits[i].commit == commitNum) {
             if(direction == "Next") {
-                newState.commit = data[i+1].commit;
-                newState.author = data[i+1].author;
-                newState.date = data[i+1].date;
-                newState.comment = data[i+1].comment;
-                newState.content = data[i+1].content;
+                newState.commit = data.commits[i+1].commit;
+                newState.author = data.commits[i+1].author;
+                newState.date = data.commits[i+1].date;
+                newState.comment = data.commits[i+1].comment;
+                newState.content = data.commits[i+1].content;
             } else if(direction == "Back") {
-                newState.commit = data[i-1].commit;
-                newState.author = data[i-1].author;
-                newState.date = data[i-1].date;
-                newState.comment = data[i-1].comment;
-                newState.content = data[i-1].content;
+                newState.commit = data.commits[i-1].commit;
+                newState.author = data.commits[i-1].author;
+                newState.date = data.commits[i-1].date;
+                newState.comment = data.commits[i-1].comment;
+                newState.content = data.commits[i-1].content;
             }
         }
     }
 
-    return(newState);
+    return newState;
 }
 
 function findData(text, keyword) {
@@ -95,11 +72,12 @@ function findCommitNumbers(text) {
     return commits;
 }
 
-fs.exists('.git', function (exists) {
+fs.exists(__dirname + '/.git', function (exists) {
     if(exists) {
         async.waterfall([
             function getCommitsInfo(callback) {
-                cp.exec('git log', function(error, stdout, stderr) {
+                cp.exec('git log', {cwd: __dirname}, function(error, stdout, stderr) {
+                    console.error(stdout);
                     var listOfCommits = [];
                     var authors = findData(stdout, "Author:");
                     var dates = findData(stdout, "Date:");
@@ -131,7 +109,10 @@ fs.exists('.git', function (exists) {
                 var commitsData = commits;
 
                 for(i=1; i<commitsData.length; i++) {
-                    commitsData[i].content = cp.execSync('git diff' + ' ' + commitsData[i-1].commit + ' ' + commitsData[i].commit).toString('utf8');
+                    commitsData[i].content =
+                        cp.execSync(
+                            'git diff' + ' ' + commitsData[i-1].commit + ' ' + commitsData[i].commit, {cwd: __dirname}
+                        ).toString('utf8');
                 }
 
                 callback(null, commitsData);
@@ -143,9 +124,12 @@ fs.exists('.git', function (exists) {
                 data[i] = JSON.stringify(result[i]);
             }
 
-            fs.writeFileSync("patches.sousp", data);
+            jsonHeader = "{ \"name\": \"hello world\", \"commits\": [";
+            jsonFooter = "] }";
 
-            console.log(nextState("1e947f613e976945de85ae35ed923aa470f0be72"));
+            fs.writeFileSync("patches.json", jsonHeader + data + jsonFooter);
+
+            console.log(nextState("1e947f613e976945de85ae35ed923aa470f0be72", "Next"));
         });
     }
 });
